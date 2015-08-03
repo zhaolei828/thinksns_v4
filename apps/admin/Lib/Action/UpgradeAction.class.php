5ba2e62821749c3a6caf48bea46b7a932c3fb0a1
zhaolei <zhaolei@chinadrtv.com>
2015-08-03 09:25:05 +0800

    update to ts 4.1.1

--- apps/admin/Lib/Action/UpgradeAction.class.php
+++ apps/admin/Lib/Action/UpgradeAction.class.php
@@ -138,11 +138,19 @@ class UpgradeAction extends AdministratorAction
 		$this->submitAlias = '立即升级';
 		$this->pageKeyList = array('log', 'tips');
 		$this->opt['tips'] = '<pre style="color:#f00;font-size:20px">
+=====如果在线升级：<font color="#000">
 1.升级前请做好网站程序和数据备份。
 2.如果您网站经过修改，请勿使用在线升级。
 3.升级需要ThinkSNS程序文件和目录拥有可写，可读权限，升级前先确定
 4.升级前一定要做好数据备份。
 5.因各种因素，您无法使用在线升级，那么，请 <a href="' . $downUrl . '">点击这里</a>手动下载升级包进行手动升级。
+</font>
+=====如果手动升级：<font color="#000">
+1.点击上方链接，下载补丁。
+2.解压下载的补丁。
+3.将补丁覆盖到您的ThinkSNS目录下。
+4.覆盖完成，<a href="' . U('/admin/Upgrade/step3') . '">点击这里</a>完成最后的升级工作。</font>
+PS：手动升级覆盖文件后千万不要刷新本页面，直接点击上方最后升级步骤。否则数据库有缺失自行负责。
 <pre/>';
 		$this->onsubmit = 'confirm(\'确定要升级吗？\')';
 		$this->displayConfig(array(
@@ -168,7 +176,7 @@ class UpgradeAction extends AdministratorAction
 		file_put_contents(DATA_PATH . '/old.thinksns.conf.php', $oldConf);
 
 		// # 下载增量包
-		is_dir(dirname($path)) or mkdir(dirname($path), '0777', true);
+		is_dir(dirname($path)) or mkdir(dirname($path), 0777, true);
 		file_put_contents($path, file_get_contents($downUrl));
 		file_exists($path) or $this->showError('下载升级包失败，请检查' . dirname($path) . '目录是否可写，如果可写，请刷新重试！');
 
@@ -188,7 +196,7 @@ class UpgradeAction extends AdministratorAction
 		foreach ($list as $info) {
 			$filename = SITE_PATH . '/' . $info['filename'];
 			if (!file_exists($filename)) {
-				is_dir(dirname($filename)) or mkdir(dirname($filename), '0777', true);
+				is_dir(dirname($filename)) or mkdir(dirname($filename), 0777, true);
 				is_dir(dirname($filename)) or $this->showError('目录' . dirname($filename) . '创建失败，请赋予0777权限');
 				file_put_contents($filename, $info['data']);
 				file_exists($filename) or $this->showError($filename . '文件写入失败，请赋予' . dirname($filename) . '目录0777权限');
@@ -227,7 +235,7 @@ class UpgradeAction extends AdministratorAction
 			$path = SITE_PATH . '/' . $path;
 			$dir  = dirname($path);
 
-			is_dir($dir) or mkdir($dir, '0777', true);
+			is_dir($dir) or mkdir($dir, 0777, true);
 			file_put_contents($path, $data);
 			unset($data);
 		}
@@ -250,7 +258,7 @@ class UpgradeAction extends AdministratorAction
 		if (file_exists($delFile)) {
 			$delFile = include $delFile;
 			foreach ($delFile as $filename) {
-				$filename = SITE_PATH . '/' . $filename;
+				$this->rm($filename, true);
 				unlink($filename);
 			}
 		}
@@ -270,6 +278,52 @@ class UpgradeAction extends AdministratorAction
 		A('Tool', 'Admin')->cleancache();
 		ob_end_clean();
 		$this->showMessage('升级成功', 'success', U('admin/upgrade/check'));
+	}
+
+	/**
+	 * 删除函数，自带递归
+	 *
+	 * @param string $path 删除的地址
+	 * @param bool $recursive 是否递归 [true]
+	 * @return bool
+	 * @author Medz Seven <lovevipdsw@vip.qq.com>
+	 **/
+	protected function rm($path, $recursive = true)
+	{
+		// # 判断是否是文件
+		if (is_file($path)) {
+			return unlink($path);
+
+		// # 判断是否是目录，判断是否递归，不递归，直接尝试删除
+		} elseif (is_dir($path) and !$recursive) {
+			return rmdir($path);
+
+		// # 判断是否是目录，如果是目录，则递归删除
+		} elseif (is_dir($path)) {
+			// # 打开目录资源
+			$handle = opendir($path);
+
+			// # 单条读取内容
+			while(false !== ($file = readdir($handle))) {
+				// # 判断内容是否是 .|.. 如果是，跳过
+				if ($file == '.' or $file == '..') {
+					// # 跳过当前循环
+					continue;
+				}
+
+				// # 递归执行自己
+				$this->rm($path . '/' . $file, $recursive);
+			}
+
+			// # 关闭目录资源
+			closedir($handle);
+
+			// # 尝试删除当前目录
+			return rmdir($path);
+		}
+
+		// # 如果以上都不满足，则返回真
+		return true;
 	}
 
 } // END class UpgradeAction extends AdministratorAction
