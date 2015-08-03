b1f10270f81852f6a794fe475957663a0733efab
zhaolei <zhaolei@chinadrtv.com>
2015-08-03 09:57:31 +0800

    haha 成功了 微博收藏导入插件

--- addons/plugin/SinaWeiboFavoritesImport/lib/config.php
+++ addons/plugin/SinaWeiboFavoritesImport/lib/config.php
@@ -3,5 +3,6 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define( "WB_AKEY" , '4154048779' );
 define( "WB_SKEY" , '2f51b6c6a1fdd2df45c3fdd4a9c0e09f' );
-define( "WB_CALLBACK_URL" , 'http://6jlife.com/callback' );
+//define( "WB_CALLBACK_URL" , 'http://6jlife.com/callback' );
 //define( "WB_CALLBACK_URL" , Addons::adminPage('importFavoritesWeibo') );
+define( "WB_CALLBACK_URL" , Addons::createAddonShow('SinaWeiboFavoritesImport','importFavoritesWeibo'));
